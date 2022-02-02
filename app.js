d5fae73ac78f229ac9634a21de0e7fd45d5088ad
zenlex <erichrkeil@gmail.com>
2022-02-02 12:52:52 -0600

    Refactor WIP
    - created blog controller
    - refactor index.js and app.js to use new controller

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
-
+const notesRouter = require('./controllers/blogs')
 const mongoUrl = process.env.MONGO_URL;
 mongoose.connect(mongoUrl);
 
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/blogs/', notesRouter)
 module.exports = app;
--- index.js
+++ index.js
@@ -1,31 +1,7 @@
 require('dotenv').config();
 const http = require('http');
-const mongoose = require('mongoose');
 const app = require('./app')
-const Blog = require('./models/blog')
 const server = http.createServer(app);
-
-
-
-
-
-app.get('/api/blogs', (req, res) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      res.json(blogs)
-    })
-});
-
-app.post('/api/blogs', (req, res) => {
-  const blog = new Blog(req.body)
-
-  blog
-    .save()
-    .then(result => {
-      res.status(201).json(result)
-    })
-});
 
 const PORT = process.env.PORT || 3003
 server.listen(PORT, () => {
