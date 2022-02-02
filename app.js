deb1223ba6d48b84f36298da6888f7e83a779812
zenlex <erichrkeil@gmail.com>
2022-02-02 13:32:06 -0600

    Refactor 4.2complete
    -create middleware util
    -create logger util
    -move basic logging and error handling functions to the utils

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose')
-const notesRouter = require('./controllers/blogs')
-const config = require('./utils/config')
+const mongoose = require('mongoose');
+const notesRouter = require('./controllers/blogs');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
 
+logger.info(`Connecting to MongoDB`);
 mongoose.connect(config.MONGO_URL)
+  .then(() => {
+    logger.info(`connected to MongoDB`)
+  })
+  .catch(err => {
+    logger.error('error connecting to MongoDB:', err.message)
+  })
 
 app.use(cors());
-app.use(express.static('build'))
+app.use(express.static('build'));
 app.use(express.json());
+app.use(middleware.requestLogger)
 
-app.use('/api/blogs/', notesRouter)
+app.use('/api/blogs/', notesRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
 module.exports = app;
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 const http = require('http');
-const app = require('./app')
+const app = require('./app');
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 3003
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 });
