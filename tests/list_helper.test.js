fa6d748a7d624b60cbb92cadb5c7d77264cde76e
zenlex <erichrkeil@gmail.com>
2022-02-03 21:10:48 -0600

    ex 4.6 complete

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,55 +1,5 @@
 const listHelper = require('../utils/list_helper');
-
-const testBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0,
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0,
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0,
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0,
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0,
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0,
-  },
-];
+const testBlogs = require('./testblogs');
 
 test('dummy returns one', () => {
   const blogs = [];
@@ -84,5 +34,26 @@ describe('favorite blog', () => {
   test('returns first blog with max likes', () => {
     const result = listHelper.favoriteBlog(testBlogs);
     expect(result).toEqual(testBlogs[2]);
+  });
+});
+
+describe('most blogs', () => {
+  test('returns one author with max titles', () => {
+    const result = listHelper.mostBlogs(testBlogs);
+    expect(result).toEqual({
+      author: 'Robert C. Martin',
+      titles: 3,
+    });
+  });
+});
+
+describe('most likes', () => {
+  test('returns object with an author who had most likes', () => {
+    expect(listHelper.mostLikes(testBlogs)).toEqual(
+      {
+        author: 'Edsger W. Dijkstra',
+        likes: 17,
+      },
+    );
   });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,4 +1,6 @@
-const dummy = (blogs) => 1;
+const _ = require('lodash');
+
+const dummy = () => 1;
 
 const totalLikes = (blogs) => {
   const result = blogs.reduce((sum, post) => sum + post.likes, 0);
@@ -11,8 +13,40 @@ const favoriteBlog = (blogs) => {
   return blogs.find((post) => post.likes === max);
 };
 
+const mostBlogs = (blogs) => {
+  const counts = _.countBy(blogs.map((blog) => blog.author));
+  const max = Math.max(...Object.values(counts));
+  const authors = Object.entries(counts)
+    .filter(([, count]) => count === max);
+  const result = {
+    author: authors[0][0],
+    titles: authors[0][1],
+  };
+  return result;
+};
+
+const mostLikes = (blogs) => {
+  const authorLikes = {};
+  blogs.forEach((blog) => {
+    const { author, likes } = blog;
+    if (authorLikes[author]) {
+      authorLikes[author] += likes;
+    } else {
+      authorLikes[author] = likes;
+    }
+  });
+  const max = Math.max(...Object.values(authorLikes));
+  const winners = Object
+    .entries(authorLikes)
+    .filter(([, likes]) => likes === max);
+  const result = { author: winners[0][0], likes: winners[0][1] };
+  return result;
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
