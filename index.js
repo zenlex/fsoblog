8e120f4b7bee6cc7b59e87a92e6c92c66d2961d3
Erich Keil <39918654+zenlex@users.noreply.github.com>
2022-02-12 17:02:04 -0600

    Https (#2)
    * convert server to https
    
    * remove keys

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const app = require('./app');
 
-const server = http.createServer(app);
+const options = {
+  key: fs.readFileSync('./key.pem'),
+  cert: fs.readFileSync('./cert.pem'),
+};
+
+const server = https.createServer(options, app);
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
