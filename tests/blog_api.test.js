44df15ba857b446766f54a99bb826e4450a7cf22
zenlex <erichrkeil@gmail.com>
2022-02-11 20:55:04 -0600

    part4 complete with tests

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,13 +1,30 @@
 /* eslint-disable no-underscore-dangle */
 const mongoose = require('mongoose');
 const supertest = require('supertest');
+const bcrypt = require('bcrypt');
 const app = require('../app');
 const Blog = require('../models/blog');
+const User = require('../models/user');
 const helper = require('./test_helper');
 
 const api = supertest(app);
-
+let token;
 const testblogs = require('./testblogs');
+
+beforeAll(async () => {
+  await User.deleteMany({});
+
+  const passwordHash = await bcrypt.hash('sekret', 10);
+  const user = new User({ username: 'root2', name: 'test user', passwordHash });
+
+  await user.save();
+
+  const response = await api
+    .post('/api/login')
+    .send({ username: 'root2', password: 'sekret' });
+
+  token = response.body.token;
+});
 
 beforeEach(async () => {
   await Blog.deleteMany({});
@@ -19,7 +36,9 @@ beforeEach(async () => {
 
 describe('get all blogs', () => {
   test('returns correct number of blogs', async () => {
-    const response = await api.get('/api/blogs');
+    const response = await api
+      .get('/api/blogs')
+      .set('Authorization', `bearer ${token}`);
 
     expect(response.body).toHaveLength(testblogs.length);
   });
@@ -27,6 +46,7 @@ describe('get all blogs', () => {
   test('returns JSON', async () => {
     await api
       .get('/api/blogs')
+      .set('Authorization', `bearer ${token}`)
       .expect(200)
       .expect('Content-Type', /application\/json/);
   });
@@ -35,7 +55,8 @@ describe('get all blogs', () => {
 describe('id property', () => {
   test('is named "id" not "_id" and __v deleted', async () => {
     const response = await api
-      .get('/api/blogs');
+      .get('/api/blogs')
+      .set('Authorization', `bearer ${token}`);
 
     const blogToCheck = response.body[0];
     expect(blogToCheck.id).toBeDefined();
@@ -44,8 +65,7 @@ describe('id property', () => {
   });
 });
 
-// TODO: fix test to work with token auth or replace with E2E
-describe.skip('post new blog', () => {
+describe('post new blog', () => {
   test('new blog added with correct content', async () => {
     const blogsAtStart = await helper.blogsInDb();
 
@@ -57,6 +77,7 @@ describe.skip('post new blog', () => {
 
     await api
       .post('/api/blogs')
+      .set('Authorization', `bearer ${token}`)
       .send(testBlog)
       .expect(201);
 
@@ -68,8 +89,7 @@ describe.skip('post new blog', () => {
   });
 });
 
-// TODO: fix test to work with token auth or replace with E2E
-describe.skip('default values', () => {
+describe('default values', () => {
   test('likes set to 0 if not initialized', async () => {
     const testBlog = {
       title: 'blog for testing no likes',
@@ -77,6 +97,7 @@ describe.skip('default values', () => {
     };
     const response = await api
       .post('/api/blogs')
+      .set('Authorization', `bearer ${token}`)
       .send(testBlog);
 
     const addedBlog = response.body;
@@ -92,6 +113,7 @@ describe('required values', () => {
 
     await api
       .post('/api/blogs')
+      .set('Authorization', `bearer ${token}`)
       .send(testBlog)
       .expect(400);
   });
@@ -99,30 +121,39 @@ describe('required values', () => {
 
 describe('delete blog', () => {
   test('successful with valid id', async () => {
-    const blogToDelete = new Blog({
-      title: 'blog to be deleted',
+    const blogToDelete = {
+      title: 'blog to be deleted now with user',
       author: 'foo bar baz',
-    });
-    const savedBlog = await blogToDelete.save();
+    };
+    const saveResponse = await api
+      .post('/api/blogs/')
+      .set('Authorization', `bearer ${token}`)
+      .send(blogToDelete)
 
     const blogsAfterAdd = await helper.blogsInDb();
+    const url = `/api/blogs/${saveResponse.body.id}`;
     await api
-      .delete(`/api/blogs/${savedBlog.id}`)
+      .delete(url)
+      .set('Authorization', `bearer ${token}`)
       .expect(200);
 
     const blogsAfterDelete = await helper.blogsInDb();
 
     expect(blogsAfterDelete).toHaveLength(blogsAfterAdd.length - 1);
-  });
+  }, 100000);
 });
 
 describe('update blog', () => {
   test('successful with valid id', async () => {
-    const blogToUpdate = new Blog({
+    const blogToUpdate = {
       title: 'blog to be updated',
       author: 'foo bar baz',
-    });
-    const savedBlog = await blogToUpdate.save();
+    };
+
+    const saveResponse = await api
+      .post('/api/blogs/')
+      .set('Authorization', `bearer ${token}`)
+      .send(blogToUpdate)
 
     const updates = {
       title: 'updated title',
@@ -130,18 +161,19 @@ describe('update blog', () => {
     };
 
     await api
-      .put(`/api/blogs/${savedBlog.id}`)
+      .put(`/api/blogs/${saveResponse.body.id}`)
+      .set('Authorization', `bearer ${token}`)
       .send(updates)
       .expect(200);
 
     const blogsAfterUpdate = await helper
       .blogsInDb();
-    const updatedBlog = blogsAfterUpdate.filter((blog) => blog.id === savedBlog.id)[0];
+    const updatedBlog = blogsAfterUpdate.filter((blog) => blog.id === saveResponse.body.id)[0];
 
     expect(updatedBlog.title).toBe(updates.title);
     expect(updatedBlog.likes).toBe(updates.likes);
   });
-});
+}, 100000);
 
 afterAll(() => {
   mongoose.connection.close();
