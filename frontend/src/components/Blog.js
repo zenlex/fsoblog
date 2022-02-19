026c5958949a85ed8d7d4462bccc9b1f5984034e
zenlex <erichrkeil@gmail.com>
2022-02-18 19:17:34 -0600

    ex 5.7 complete blog details

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+
+  const [showDetails, setShowDetails] = useState(false);
+
+
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        <span style={{ marginRight: 10 }}> {blog.title}</span>
+        <span style={{ marginRight: 10 }}>{blog.author}</span>
+        <button onClick={() => setShowDetails(!showDetails)}>{showDetails ? 'hide' : 'view'}</button>
+      </div>
+      {
+        showDetails && <div>
+          <p>{blog.url}</p>
+          <span>
+            {blog.likes}
+            <button onClick={() => console.log('add a like')}>like</button>
+          </span>
+          <p>{blog.user.name}</p>
+        </div>
+      }
+    </div >
+  )
+}
 
 export default Blog
