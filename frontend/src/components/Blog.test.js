70e43c1a686bdbaf1c652a056e8090487f1008b4
zenlex <erichrkeil@gmail.com>
2022-02-21 15:01:55 -0600

    add blog details click test

--- frontend/src/components/Blog.test.js
+++ frontend/src/components/Blog.test.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders title and author only', async () => {
+test('renders title and author only', () => {
   const blog = {
     title: 'TestBlog',
     author: 'TestAuthor',
@@ -19,9 +20,37 @@ test('renders title and author only', async () => {
 
   const titleElement = screen.getByText('Title: TestBlog', { exact: false })
   const authorElement = screen.getByText('Author: TestAuthor', { exact: false })
-  const url = await screen.queryByText('http://dontshowme.com')
+  const url = screen.queryByText('http://dontshowme.com')
 
   expect(titleElement).toBeDefined()
   expect(authorElement).toBeDefined()
   expect(url).toBeNull()
+})
+
+
+test('clicking details button shows likes and url', async () => {
+  const blog = {
+    title: 'TestBlog',
+    author: 'TestAuthor',
+    likes: 42,
+    url: 'http://dontshowme.com',
+    user: { name: 'testUser', id: '12345' },
+  }
+
+
+  render(<Blog blog={blog}
+    updateBlog={() => console.log('updateBlog')}
+    deleteBlog={() => console.log('deleteBlog')}
+    currUser={{ name: 'testUser', id: '12345' }}
+  />)
+
+  const button = screen.getByText('view');
+
+  userEvent.click(button)
+
+  const likes = screen.getByText('Likes:', { exact: false });
+  const url = screen.getByText('URL', { exact: false })
+
+  expect(likes).toBeDefined()
+  expect(url).toBeDefined()
 })
