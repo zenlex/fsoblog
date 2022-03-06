982b8b89f903d6709f03380c530eb42172a4ebfa
zenlex <erichrkeil@gmail.com>
2022-03-06 10:54:42 -0600

    sketch in store and configure prettier

---
+++ frontend/src/reducers.js
@@ -0,0 +1,14 @@
+export const blogReducer = (state, action) => {
+  console.log('blogReducer called', action);
+  return state;
+};
+
+export const userReducer = (state, action) => {
+  console.log('userReducer called', action);
+  return state;
+};
+
+export const alertReducer = (state, action) => {
+  console.log('alertReducer called', action);
+  return state;
+};
---
+++ frontend/src/store.js
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { blogReducer, userReducer, alertReducer } from './reducers';
+
+const initialState = {
+  blogs: [],
+  user: null,
+  notification: null,
+};
+
+const store = configureStore({
+  name: 'blogstore',
+  initialState,
+  reducers: {
+    blogs: blogReducer,
+    users: userReducer,
+    alerts: alertReducer,
+  },
+});
+
+export default store;
