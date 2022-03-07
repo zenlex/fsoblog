efb69ae6d8b4a38ab0b228960f2b673fc5adce78
zenlex <erichrkeil@gmail.com>
2022-03-07 17:30:26 -0600

    cypress tests fixed

--- frontend/cypress/integration/blog_spec.js
+++ frontend/cypress/integration/blog_spec.js
@@ -1,101 +1,91 @@
 describe('Blog app', function () {
   beforeEach(function () {
-    cy.request('POST', 'https://localhost:3003/api/testing/reset')
+    cy.request('POST', 'https://localhost:3003/api/testing/reset');
     cy.request('POST', 'https://localhost:3003/api/users', {
-      username: 'codemonkey', password: 'foobar', name: 'Test User'
-    })
-    cy.visit('https://localhost:3000')
-    window.sessionStorage.removeItem('blogAppUser')
-  })
+      username: 'codemonkey',
+      password: 'foobar',
+      name: 'Test User',
+    });
+    cy.visit('https://localhost:3000');
+    window.sessionStorage.removeItem('blogAppUser');
+  });
 
   it('Login form is shown', function () {
-    cy.contains('login')
-    cy.contains('username')
-    cy.contains('password')
+    cy.contains('login');
+    cy.contains('username');
+    cy.contains('password');
 
-    cy.get('html').should('not.contain', 'Logout')
-  })
+    cy.get('html').should('not.contain', 'Logout');
+  });
 
   describe('Login', function () {
     it('succeeds with correct credentials', function () {
-      cy.loginForm('codemonkey', 'foobar')
-      cy.get('html').should('contain', 'login successful')
-    })
+      cy.loginForm('codemonkey', 'foobar');
+      cy.get('html').should('contain', 'login successful');
+    });
     it('fails with incorrect credentials', function () {
-      cy.loginForm('wrong', 'morewrong')
-      cy.get('html').should('not.contain', 'login successful')
-      cy.contains('invalid').should('have.css', 'color', 'rgb(255, 0, 0)')
-    })
-  })
+      cy.loginForm('wrong', 'morewrong');
+      cy.get('html').should('not.contain', 'login successful');
+      cy.contains('invalid').should('have.css', 'color', 'rgb(255, 0, 0)');
+    });
+  });
 
   describe('While logged in', function () {
     beforeEach(function () {
-      cy.loginBlind('codemonkey', 'foobar')
-    })
+      cy.loginBlind('codemonkey', 'foobar');
+      cy.visit('https://localhost:3000/blogs');
+    });
     it('should show logout button', function () {
-      cy.get('html').should('not.contain', 'Logout')
-    })
+      cy.get('html').should('contain', 'Logout');
+    });
 
     it('user can add new blog', function () {
-      cy.addBlog('blog by cypress', 'Sy Press', 'http://localhost.com')
+      cy.addBlog('blog by cypress', 'Sy Press', 'http://localhost.com');
 
-      cy.get('html').should('contain', 'blog by cypress')
-    })
+      cy.get('html').should('contain', 'blog by cypress');
+    });
 
     it('user can like a blog', function () {
-      cy.addBlog('likable', 'Sy press', 'likes.com')
-      cy.contains('likable').parent().parent().as('blogContainer')
-      cy.get('@blogContainer')
-        .contains('view')
-        .click()
+      cy.addBlog('likable', 'Sy press', 'likes.com');
+      cy.contains('likable').click();
 
-      cy.get('[data-cy=likes]')
-        .should('contain', 'Likes: 0')
+      cy.get('[data-cy=likes]').should('contain', 'Likes: 0');
 
-      cy.get('@blogContainer')
-        .get('[data-cy=like-btn]')
-        .click()
+      cy.get('[data-cy=like-btn]').click();
 
-      cy.get('@blogContainer')
-        .should('contain', 'Likes: 1')
-    })
+      cy.get('[data-cy=likes]').should('contain', 'Likes: 1');
+    });
 
     it('user can delete a blog', function () {
-      cy.addBlog('likable', 'Sy press', 'likes.com')
-      cy.contains('likable').parent().parent().as('blogContainer')
-      cy.get('@blogContainer')
-        .contains('view')
-        .click()
+      cy.addBlog('likable', 'Sy press', 'likes.com');
+      cy.contains('likable').click();
 
-      cy.get('@blogContainer')
-        .get('[data-cy=delete]')
-        .click()
+      cy.get('[data-cy=delete]').click();
 
-      cy.get('html').should('not.contain', 'likable')
-    })
+      cy.get('html').should('not.contain', 'likable');
+    });
 
     it('cannot delete a blog by another user', function () {
       // create blog to delete
-      cy.addBlog('undeletable', 'Sy press', 'likes.com')
-      cy.contains('Logout').click()
+      cy.addBlog('undeletable', 'Sy press', 'likes.com');
+      cy.contains('Logout').click();
 
       // create 2nd user
       cy.request('POST', 'https://localhost:3003/api/users', {
-        username: 'user2', password: 'foobar', name: 'Test User 2'
-      })
-      cy.loginBlind('user2', 'foobar')
+        username: 'user2',
+        password: 'foobar',
+        name: 'Test User 2',
+      });
+      cy.loginBlind('user2', 'foobar');
 
       // open created blog
-      cy.contains('undeletable').parent().parent().as('blogContainer')
-      cy.get('@blogContainer')
-        .contains('view')
-        .click()
+      cy.visit('https://localhost:3000/blogs');
+      cy.contains('undeletable').click();
 
       // should not have delete button
-      cy.get('@blogContainer')
-        .get('[data-cy=delete]').should('not.exist')
-    })
+      cy.get('[data-cy=delete]').should('not.exist');
+    });
 
     // TODO: Optional 5.22* add testing to ensure blogs are sorted correctly by likes
-  })
-})
+  });
+});
